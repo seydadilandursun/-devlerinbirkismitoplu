b47266da523f99195f71d326b33ceae50b9cd053
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-26 21:59:21 +0300

    ödev11 ve ödev12 eklendi

---
+++ odev1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿//1.Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
+//Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+// Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+
+Console.WriteLine("Lüften pozitif bir sayı giriniz:");
+int n= int.Parse(Console.ReadLine());
+
+
+int[] sayilar2= new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Lütfen {0} ıncı sayıyı giriniz adet sayı giriniz:",i+1);  
+    sayilar2[i]=int.Parse(Console.ReadLine());
+}
+
+foreach (int sayi in sayilar2)
+{
+    if(sayi%2==0)
+      Console.WriteLine(sayi);
+}
---
+++ odev1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//2.Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
+//Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+//Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+Console.WriteLine("Lüften pozitif bir sayı giriniz:");
+int k= int.Parse(Console.ReadLine());
+Console.WriteLine("Lüften pozitif bir sayı giriniz:");
+int m= int.Parse(Console.ReadLine());
+int[] sayilar2= new int[k];
+for (int i = 0; i < k; i++)
+{
+    Console.WriteLine("Lütfen {0} ıncı sayıyı giriniz adet sayı giriniz:",i+1);  
+    sayilar2[i]=int.Parse(Console.ReadLine());
+}
+
+foreach (int sayi in sayilar2)
+{
+    if(sayi%m==0)
+      Console.WriteLine(sayi);
+}
+
