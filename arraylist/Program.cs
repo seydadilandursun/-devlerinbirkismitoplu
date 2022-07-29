ef71c33612b5624b0f2ab532a250c4770753229c
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-29 16:14:47 +0300

    array list ders kodlari yazildi

---
+++ arraylist/Program.cs
@@ -0,0 +1,54 @@
+﻿//System.Collections namespace
+using System.Collections;
+
+ArrayList list=new ArrayList();
+//list.Add("Ayse");
+//list.Add(2);
+//list.Add(true);
+//list.Add('A');
+
+//Icerisinde verilere erisim
+//Console.WriteLine(list[1]);
+//foreach (var item in list)
+//{
+//    Console.WriteLine(item);
+//}
+
+//AddRange
+Console.WriteLine("***Add Range***");
+//string[] renkler={"kirmizi","sari","yesil"};
+List<int> sayilar= new List<int>(){1,8,3,7,9,92,5};
+//list.AddRange(renkler);
+list.AddRange(sayilar);
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+//Sort
+Console.WriteLine("***Sort ***");
+list.Sort();
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+//Binary Search 
+Console.WriteLine("***Binary Search ***");
+Console.WriteLine(list.BinarySearch(9));
+
+//Reverse
+Console.WriteLine("***Reverse ***");
+list.Reverse();
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+//clear
+Console.WriteLine("***Clear***");
+list.Clear();
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
