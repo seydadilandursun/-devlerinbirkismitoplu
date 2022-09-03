f6b285e21865e49b581b81967d8b96b2b023cd4d
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-09-03 20:09:28 +0300

    Enum Kavramı

---
+++ enumkavramı/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Cumartesi);
+int sicaklik=32;
+
+if(sicaklik<= (int)HavaDurumu.Normal)
+    Console.WriteLine("Disariya cikmak icin havanin biraz daha isinmasini bekleyin.");
+else if(sicaklik>=(int)HavaDurumu.Sicak)
+    Console.WriteLine("Disariya cikmak icin cok sicak bir gun.");
+else if(sicaklik>=(int)HavaDurumu.Normal&&sicaklik<=(int)HavaDurumu.CokSicak)
+    Console.WriteLine("Hadi disari cikalim.");
+    
+     
+
+
+
+enum Gunler
+{
+    Pazartesi=1,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma=23,
+    Cumartesi,
+    Pazar
+}
+
+
+enum HavaDurumu
+{
+    Soguk=5,
+    Normal=20,
+    Sicak=25,
+     CokSicak=30,
+
+
+}
