52b0873550a638db1847d0d873f29dd52f73a47b
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-09-03 19:40:54 +0300

    StructKavrami

---
+++ structkavramı/Program.cs
@@ -0,0 +1,55 @@
+﻿Dikdortgen dikdortgen = new Dikdortgen();
+//dikdortgen.KisaKenar= 3;
+//dikdortgen.UzunKenar= 4;
+Console.WriteLine("Class Alan Hesabi: {0}",dikdortgen.AlanHesabi());
+
+//Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct();
+//dikdortgen_struct.KisaKenar= 3;
+//dikdortgen_struct.UzunKenar= 4;
+
+//Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct(3,4);
+//Console.WriteLine("Struct Alan Hesabi: {0}",dikdortgen_struct.AlanHesapla());
+Dikdortgen_Struct dikdortgen_struct;
+//dikdortgen_struct.KisaKenar= 3;
+//dikdortgen_struct.UzunKenar= 4;
+Console.WriteLine("Struct Alan Hesabi: {0}",dikdortgen_struct.AlanHesapla());
+
+
+
+
+
+class Dikdortgen
+{
+    public int KisaKenar;
+
+    public int UzunKenar;
+
+  //  public Dikdortgen()
+  //  {
+  //      KisaKenar=3;
+  //      UzunKenar=4;
+  //  }
+    public long AlanHesabi()
+    {
+        return this.KisaKenar*this.UzunKenar;
+    }
+}
+struct Dikdortgen_Struct
+{
+       public int KisaKenar;
+       public int UzunKenar;
+
+    public Dikdortgen(int kisaKenar, int uzunKenar)
+    {
+        KisaKenar = 3;
+        UzunKenar = 4;
+        KisaKenar = kisaKenar;
+        UzunKenar = uzunKenar;
+    }
+    public long AlanHesapla()
+       {
+         return this.KisaKenar*this.UzunKenar;
+       }
+
+
+}
